81305608fb44d7b54abd0df434736ea619f6bb6f
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-22 15:43:54 +0530

    done

--- Test.java
+++ Test.java
@@ -3,6 +3,9 @@ public class Test{
         
         ReturnType in=new ReturnType();
         in.reve(8);
+
+        Voidtype in1=new Voidtype();
+        in1.prime(9);
 // Call Three Functions and Implement Here
 }
 }
