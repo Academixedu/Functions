40af75f7b1f4d554a1f10218dea63c6ba9d562cc
kurojuLucky <luckykuroju28@gmail.com>
2024-08-22 20:57:39 +0530

    lucky

--- Test.java
+++ Test.java
@@ -1,3 +1,25 @@
 public class Test{
-// Call Three Functions and Implement Here
+        // Call Three Functions and Implement Here
+        public int add(int a, int b) {
+            int sum = a+b;  
+            return sum; 
+        }
+        public int multiple(int a, int b) {
+            int multiply = a*b;  
+            return multiply; 
+        }
+        public int sub(int a, int b) {
+            int sub = a%b;  
+            return sub; 
+        }
+        
+        public static void main(String[] args) {
+            Test ok = new Test();
+            int l = ok.add(5, 7);
+            System.out.println(l);
+            int l2 = ok.multiple(2, 5);
+            System.out.println(l2);
+            int l3 = ok.sub(10, 9);
+            System.out.println(l3);
+        }
 }
