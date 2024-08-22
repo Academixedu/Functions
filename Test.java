ed5025f30f3a5b24cdc0fd68bcb8f5d2df80a7da
karthhiik <karthikthatikonda18@gmail.com>
2024-08-22 19:46:42 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,3 +1,25 @@
 public class Test{
 // Call Three Functions and Implement Here
+  public void hello(){
+  System.out.println("Hello world');
+  }
+  public static void hi(){
+   System.out.println("its a hi function):
+   int i = 10;
+   int a = 20;
+   int sum = int i + int a;
+  }
+  public int hey(){
+   int sum = 2+9;
+    System.out.println("Sum"+sum);
+    return sum;
+  }
+  public static void main(String[] ars){
+    Test ts=new Test();
+    int sum=ts.hey();
+    System.out.println("this is hey sum"+sum);
+    hi();
+    ts.hello();
+    
+  }
 }
