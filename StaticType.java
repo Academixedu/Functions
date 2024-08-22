c058cd9a4fb41c082b504e88bab7b97802f401a0
kurojuLucky <luckykuroju28@gmail.com>
2024-08-22 20:59:23 +0530

    l

--- StaticType.java
+++ StaticType.java
@@ -1,23 +1,3 @@
 public class StaticType{
     // Load 5 Numbers using an Array and Check whether every Number is Prime or not
-    public static boolean isPrime(int n)
-    {
-        if (n <= 1)
-            return false;
-        for (int i = 2; i <= Math.sqrt(n); i++)
-            if (n % i == 0)
-                return false;
-
-        return true;
-    }
-    public static void main(String args[])
-    {
-        int num = 9;
-        if (isPrime(num)) {
-            System.out.println(num + " is prime");
-        }
-        else {
-            System.out.println(num + " is not prime");
-        }
-    }
 }
