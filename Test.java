02b92f569f8adad73436810b7e33d4c65f7a3ca0
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-22 15:19:06 +0530

    done

--- Test.java
+++ Test.java
@@ -1,3 +1,8 @@
 public class Test{
+    public static void main(String[] args) {
+        
+        ReturnType in=new ReturnType();
+        in.reve(8);
 // Call Three Functions and Implement Here
+}
 }
--- Voidtype.java
+++ Voidtype.java
@@ -1,3 +1,14 @@
 public class Voidtype {
+    public void odd( int a,String s)
+{
+if (a%2==0) {
+System.out.println("even"+s);
+    
+}
+else{
+    System.out.println("odd"+s);
+}
+
+}
 // Prepare a Void Function to check whether a number is prime or not with a parameter of int
 }
