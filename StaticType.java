bc6bd74f5da18136a3a12dc6e51516abb1340e95
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-08-22 17:14:39 +0530

    Revathi

--- StaticType.java
+++ StaticType.java
@@ -1,3 +1,27 @@
-public class StaticType{
+public class StaticType {
 // Load 5 Numbers using an Array and Check whether every Number is Prime or not
+    public void prime(int a){
+        if(a<=1){
+            System.out.println("prime number ");
+        }
+        int count=0;
+        for(int i=1;i<=a;i++){
+            if(a%i==0){
+                count++;
+            }
+        }
+        if(count==2){
+            System.out.println("prime number");
+        }
+         else{
+             System.out.println("not a prime number");
+        }
+    }
+    public static void main(String[] args) {
+        StaticType st=new StaticType();
+        int[]array={13,14,15,16,17};
+        for(int num:array){
+            st.prime(num);
+        }
+    }
 }
--- Voidtype.java
+++ Voidtype.java
@@ -1,3 +1,24 @@
 public class Voidtype {
-// Prepare a Void Function to check whether a number is prime or not with a parameter of int
+    int count=0;
+    public void prime(int a){
+        if(a<=1){
+            System.out.println("prime number ");
+        }
+        for(int i=1;i<=a;i++){
+            if(a%i==0){
+                count++;
+            }
+        }
+        if(count==2){
+             System.out.println("prime numeber");
+        }
+         else{
+             System.out.println("not a prime number");
+        }
+    }  
+    public static void main(String[]args){
+        Voidtype v=new Voidtype();
+        v.prime(9);
+    }
 }
+
