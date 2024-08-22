83a97ef0fbfe09e255a93e1745bb3dab03814430
SMohammadKhan <iammohammad65@gmail.com>
2024-08-22 11:58:44 +0530

    return

--- ReturnType.java
+++ ReturnType.java
@@ -1,4 +1,25 @@
 public class ReturnType {
-// Prepare a Logic to reverse a number and return the reversed number
-  
+
+    public static int reverseNumber(int number) {
+        int reversed = 0;
+        int originalNumber = number;
+        while (number != 0) {
+            int digit = number % 10; 
+            reversed = reversed * 10 + digit;
+            number /= 10; 
+        }
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        int number = 12345;
+        int reversed = reverseNumber(number);
+        System.out.println("Original Number: " + number);
+        System.out.println("Reversed Number: " + reversed);
+        
+        number = -6789;
+        reversed = reverseNumber(number);
+        System.out.println("Original Number: " + number);
+        System.out.println("Reversed Number: " + reversed);
+    }
 }
