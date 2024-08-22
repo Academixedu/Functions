ec7de7646586522a5d3464d6c6a37284e70ec1e7
Aparna526 <aparnakondapaturu@gmail.com>
2024-08-22 13:36:50 +0530

    Functions

--- StaticType.java
+++ StaticType.java
@@ -4,11 +4,25 @@ public class StaticType{
 // Load 5 Numbers using an Array and Check whether every Number is Prime or not
 
 public static void EvenOdd(int number){
-    if (number%2==0) {
-        System.out.println("Even");        
+    int count=0;
+    if(number==1){
+        System.out.println("Not a Prime number");
+    }
+    else if(number>1){
+        for(int i=2;i<number;i++){
+            if(number%i==0){
+                count=1;
+            }
+        }
+        if(count==0){
+            System.out.println("prime");
+        }
+        else{
+            System.out.println("Not Prime");
+        }
     }
     else{
-        System.out.println("Odd");
+        System.out.println("Not a number");
     }
 }
 
