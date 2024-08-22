5611ad8a7408f172bfb404a334dcaf97c785549d
Arbaz-01 <arbazshaibu@gmail.com>
2024-08-22 19:27:02 +0530

    arbazzzzz

--- Voidtype.java
+++ Voidtype.java
@@ -1,3 +1,24 @@
 public class Voidtype {
-// Prepare a Void Function to check whether a number is prime or not with a parameter of int
+    public  void prime(int a,String P, String NP) {
+        if (a<=1) {
+            System.out.println(a+" :"+NP );
+        }
+        else if (a<=3) {
+            System.out.println(a+" :"+P);
+        }
+        else if (a%2==0 || a%3==0) {
+            System.out.println(a+" :"+NP );
+        }
+        else{
+        for (int i = 5; i * i <= a; i += 6) {
+            if (a % i == 0 || a % (i + 2) == 0) {
+                System.out.println(a+" :"+NP );
+              return;
+    }
 }
+System.out.println(a+" :"+P);
+}
+    }
+}
+// Prepare a Void Function to check whether a number is prime or not with a parameter of int
+
---
+++ Voidtype1.java
@@ -0,0 +1,9 @@
+public class Voidtype1 {
+    public static void main(String[] args) {
+        Voidtype o= new Voidtype();
+       o.prime(19, "is a prime number", "is not a prime number");
+       
+    }
+   
+    
+}
