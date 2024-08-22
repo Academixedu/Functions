86a56ac9b6b7852f5be5f9614824ff6ed082e500
karthhiik <karthikthatikonda18@gmail.com>
2024-08-22 19:37:36 +0530

    Update Voidtype.java

--- Voidtype.java
+++ Voidtype.java
@@ -1,3 +1,24 @@
+import java.util*;
 public class Voidtype {
-// Prepare a Void Function to check whether a number is prime or not with a parameter of int
+public void prime(){
+Scanner sc = new Scanner(System.in)
+int n = Sc.nextInt();
+int count = 0;
+  if(n==0){
+  System.out.println( " Not a prime number");
+  }
+  for(int i =1; i<n; i++){
+   if(n%i==0){
+    count++;
+   }
+  }
+  if(count==2)
+  System.out.println("Its a prime number);
+  else
+   System.out.println("Its not a prime number);
+}
+  public static void main(Strind[] args){
+   void type obj = new void type();
+    obj.prime();
+  }
 }
